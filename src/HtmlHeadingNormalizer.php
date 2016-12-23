70949a57ab8e2abb8ab15f2dd739d19bf9ba6cab
Viktor Persson <viktor.persson@arcsin.se>
2016-12-23 15:51:27 +0100

    Began working on the normalize() method (WIP), added tests.

--- src/HtmlHeadingNormalizer.php
+++ src/HtmlHeadingNormalizer.php
@@ -4,15 +4,39 @@ namespace Vikpe;
 
 class HtmlHeadingNormalizer
 {
-    public static function normalize($html)
+    public static function normalize($html, $base_level = 1)
     {
         if (!self::htmlContainsHeadings($html)) {
             return $html;
         }
 
-        $normalizedHtml = $html;
+        $domDocument = new \DOMDocument();
+        $domDocument->loadHTML($html);
 
-        return $normalizedHtml;
+        $headingTagNames = ['h1', 'h2', 'h3', 'h4', 'h6'];
+
+        $originalHeadingDomElements = [];
+        $normalizedHeadingDomElements = [];
+
+        foreach ($headingTagNames as $headingTagName) {
+            $headingDomElements = $domDocument->getElementsByTagName($headingTagName);
+
+            foreach ($headingDomElements as $headingDomElement) {
+                $currentHeadingLevel = self::headingTagNameToNumber($headingDomElement->tagName);
+                $newHeadingLevel = self::numberToHeadingLevel($base_level + $currentHeadingLevel - 1);
+
+                if ($newHeadingLevel !== $currentHeadingLevel) {
+                    $originalHeadingDomElements[] = $headingDomElement;
+                    $normalizedHeadingDomElements[] = self::cloneDomElementWithNewTagName($headingDomElement, $newHeadingLevel);
+                }
+            }
+        }
+
+        foreach ($originalHeadingDomElements as $i => $needle) {
+            $needle->parentNode->replaceChild($normalizedHeadingDomElements[$i], $needle);
+        }
+
+        return $domDocument->saveHTML();
     }
 
     private static function htmlContainsHeadings($html)
@@ -21,5 +45,39 @@ class HtmlHeadingNormalizer
         $containsHeadings = (false !== stripos($html, $headingNeedle));
 
         return $containsHeadings;
+    }
+
+    private static function headingTagNameToNumber($headingLevel)
+    {
+        return substr($headingLevel, 1);
+    }
+
+    private static function numberToHeadingLevel($number)
+    {
+        return 'h' . $number;
+    }
+
+    private static function cloneDomElementWithNewTagName(\DOMElement $sourceDomElement, $newTagName)
+    {
+        $targetDomElement = $sourceDomElement->parentNode->ownerDocument->createElement($newTagName);
+        self::copyAttributes($sourceDomElement, $targetDomElement);
+        self::moveChildNodes($sourceDomElement, $targetDomElement);
+
+        return $targetDomElement;
+    }
+
+    private static function copyAttributes(\DOMElement $source, \DOMElement $target)
+    {
+        foreach ($source->attributes as $attribute) {
+            $target->setAttribute($attribute->name, $attribute->value);
+        }
+    }
+
+    private static function moveChildNodes(\DOMElement $source, \DOMElement $target)
+    {
+        while ($source->hasChildNodes()) {
+            // appendChild() actually moves the childNode
+            $target->appendChild($source->childNodes->item(0));
+        }
     }
 }
--- tests/HtmlHeadingNormalizerTest.php
+++ tests/HtmlHeadingNormalizerTest.php
@@ -4,20 +4,54 @@ namespace Vikpe;
 
 class HtmlHeadingNormalizerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testNormalize()
+    const TEST_FILES_DIR = __DIR__ . '/file/';
+
+    private function getTestFileContents($filename)
     {
+        return file_get_contents(self::TEST_FILES_DIR . $filename);
+    }
+
+    public function assertHtmlStringEqualsHtmlString($expect, $actual)
+    {
+        $expectedDom = new \DOMDocument();
+        $expectedDom->loadHtml($expect);
+        $expectedDom->preserveWhiteSpace = false;
+
+        $actualDom = new \DOMDocument();
+        $actualDom->loadHTML($actual);
+        $actualDom->preserveWhiteSpace = false;
+
         $this->assertEquals(
-            '',
-            HtmlHeadingNormalizer::normalize('')
+            $expectedDom->saveHTML(),
+            $actualDom->saveHTML()
         );
+    }
 
-        /////
-
-        $html = '<h2>Foo</h2><p>bar</p>';
-
-        $expect = '<h2>Foo</h2><p>bar</p>';
+    /**
+     * @dataProvider normalizeDataProvider
+     */
+    public function testNormalize($html, $expect)
+    {
         $actual = HtmlHeadingNormalizer::normalize($html);
 
         $this->assertEquals($expect, $actual);
+    }
+
+    public function normalizeDataProvider()
+    {
+        return [
+            ['', ''],
+            ['<p>foo</p>', '<p>foo</p>'],
+        ];
+    }
+
+    public function testNormalize_promoteHtmlDocument()
+    {
+        $inputHtml = $this->getTestFileContents('document.base1.html');
+        $normalizedHtml = HtmlHeadingNormalizer::normalize($inputHtml, 3);
+
+        $expectedHtml = $this->getTestFileContents('document.base1.promote3.html');
+
+        $this->assertHtmlStringEqualsHtmlString($expectedHtml, $normalizedHtml);
     }
 }
